8def380968a076eb46d579fe969fb767d50b13d4
techexperttutorials <techexperttutorials@gmail.com>
2024-12-29 13:34:30 -0700

    first commit

---
+++ OpenAI_ImageContent_v2.py
@@ -0,0 +1,45 @@
+import base64
+from openai import OpenAI
+
+client = OpenAI()
+
+# Function to encode the image
+def encode_image(image_path):
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode("utf-8")
+
+
+# Path to your image
+image_path = "animals.jpg"
+
+# Getting the base64 string
+base64_image = encode_image(image_path)
+
+response = client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages=[
+        {
+            "role": "user",
+            "content": [
+                {
+                    "type": "text",
+                    "text": "What is in this image?",
+                },
+                {
+                    "type": "image_url",
+                    "image_url": {"url": f"data:image/jpeg;base64,{base64_image}"},
+                },
+            ],
+        }
+    ],
+)
+
+print(response.choices[0])
+
+import textwrap
+
+def print_wrapped(text, width=80):
+    wrapped_text = textwrap.fill(text, width=width)
+    print(wrapped_text)
+
+print_wrapped(str(response.choices[0].message))
